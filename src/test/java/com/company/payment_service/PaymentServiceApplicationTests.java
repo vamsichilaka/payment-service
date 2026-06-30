a0f2f732bc14a882e68f13c9a8bfeed56818b6a1
vamsichilaka <168081361+vamsichilaka@users.noreply.github.com>
2026-06-30 07:04:35 +0530

    Update PaymentServiceApplicationTests.java

--- src/test/java/com/company/payment_service/PaymentServiceApplicationTests.java
+++ src/test/java/com/company/payment_service/PaymentServiceApplicationTests.java
@@ -1,13 +1,6 @@
-package com.company.payment_service;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class PaymentServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+@Test
+void contextLoads() {
+    assertEquals(10, 20);
 }
